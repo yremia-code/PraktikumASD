0665af9fc76b33cec0d756ea2a340f900b3940af
YeremiaEgaWahyudi <yremiawahyudi@gmail.com>
2026-03-16 19:21:32 +0700

    Remove .vscode settings

--- 6_sorting/tempCodeRunnerFile.c
+++
@@ -1,4 +0,0 @@
-case 1:
-        //     SelectionSort(data_selection, jml);
-        //     puts(" ");
-        //     break;
--- coba.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int *ptr, a=10;
-    ptr = &a;
-    *ptr += 1;
-    printf("%d, %d", *ptr, a);
-}
